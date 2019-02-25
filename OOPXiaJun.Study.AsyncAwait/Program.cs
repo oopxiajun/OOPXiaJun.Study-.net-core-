159088d6f6c6718cb0343e146d33801ec45d7cbb
xiajun <674008411@qq.com>
2019-02-25 20:31:06 +0800

    添加项目文件。

---
+++ OOPXiaJun.Study.AsyncAwait/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOPXiaJun.Study.AsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
